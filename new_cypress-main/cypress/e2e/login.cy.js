f071bcb8cc082f1cb4b3b450663d16066b9a601f
Natalia Gnevanova <143925521+ngnevanova@users.noreply.github.com>
2025-01-03 00:10:01 +0300

    Add files via upload

---
+++ new_cypress-main/cypress/e2e/login.cy.js
@@ -0,0 +1,69 @@
+import * as data from "../helpers/default_data.json"
+import * as main_page from "../locators/main_page.json";
+import * as recovery_password_page from "../locators/recovery_password_page.json"
+import * as result_page from "../locators/result_page.json"
+
+describe('Проверка авторизации', function () {
+ 
+    beforeEach('Начало теста', function () {
+        cy.visit('/');
+          });
+
+    afterEach('Конец теста', function () {
+        cy.get(result_page.close).should('be.visible');
+          });      
+
+    it('Правильный логин и правильный пароль', function () {
+         cy.get(main_page.email).type(data.login);
+         cy.get(main_page.password).type(data.password);
+         cy.get(main_page.login_button).click();
+         cy.get(result_page.title).should('be.visible');
+         cy.get(result_page.title).contains('Авторизация прошла успешно');
+     })
+
+     it('Восстановление пароля', function () {
+        cy.get(main_page.fogot_pass_btn).click();
+        cy.get(recovery_password_page.email).type(data.login);
+        cy.get(recovery_password_page.send_button).click();
+        cy.get(result_page.title).contains('Успешно отправили пароль на e-mail');
+    })
+
+    it('Правильный логин и НЕправильный пароль', function () {
+        cy.get(main_page.email).type(data.login);
+        cy.get(main_page.password).type('iLoveqastudio2');
+        cy.get(main_page.login_button).click();
+        cy.get(result_page.title).should('be.visible');
+        cy.get(result_page.title).contains('Такого логина или пароля нет');
+    })
+
+    it('НЕправильный логин и правильный пароль', function () {
+        cy.get(main_page.email).type('german@dolniko.ru');
+        cy.get(main_page.password).type(data.password);
+        cy.get(main_page.login_button).click();
+        cy.get(result_page.title).should('be.visible');
+        cy.get(result_page.title).contains('Такого логина или пароля нет');
+    })
+
+    it('Валидация на наличие @', function () {
+        cy.get(main_page.email).type('germandolnikov.ru');
+        cy.get(main_page.password).type(data.password);
+        cy.get(main_page.login_button).click();
+        cy.get(result_page.title).should('be.visible');
+        cy.get(result_page.title).contains('Нужно исправить проблему валидации');
+    })
+
+    it('Приведение к строчным буквам в логине', function () {
+        cy.get(main_page.email).type('GerMan@Dolnikov.ru');
+        cy.get(main_page.password).type('iLoveqastudio1');
+        cy.get(main_page.login_button).click();
+        cy.get(result_page.title).should('be.visible');
+        cy.get(result_page.title).contains('Авторизация прошла успешно');
+    })
+
+ })
+
+
+ 
+ 
+
+ 
